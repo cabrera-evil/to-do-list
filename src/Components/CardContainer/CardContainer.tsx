1b6385b3883b19a3a2fa3139deef52592ced6581
cabrera-evil <douglascabrera.dev@gmail.com>
2023-03-05 00:26:05 -0600

    Update: file structure

---
+++ src/Components/CardContainer/CardContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CardContainer.css";
+
+import Card from "../Card/Card";
+
+const CardContainer: React.FC = () => {
+    return (
+        <div className="card-container">
+            <Card />
+        </div>
+    )
+};
+
+export default CardContainer;
